e627a517b2acebcd0cd740c4367df98745eb93db
castillorosy <rosycastillo18@yahoo.com>
2015-02-24 14:02:53 -0800

    idk

--- post.php
+++ post.php
@@ -3,5 +3,4 @@
 require_once(__DIR__ . "/view/header.php");
 require_once(__DIR__ . "/view/form.php");
 require_once(__DIR__ . "/view/footer.php");
-
-//<a href="logout-user.php" class="btn btn-info" role="button">Logout?</a>
+require_once(__DIR__ . "/view/logout-user.php");
